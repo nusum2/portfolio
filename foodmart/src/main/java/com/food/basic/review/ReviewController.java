a71114a1d529c03dcbf7b78d7c7fe9dae2283dc9
nusum2 <warfare804@gmail.com>
2025-01-16 03:26:47 +0900

    리뷰에 이미지 기능 추가

--- foodmart/src/main/java/com/food/basic/review/ReviewController.java
+++ foodmart/src/main/java/com/food/basic/review/ReviewController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,9 +16,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.food.basic.common.dto.Criteria;
 import com.food.basic.common.dto.PageDTO;
+import com.food.basic.common.util.FileManagerUtils;
 import com.food.basic.user.UserVO;
 
 import jakarta.servlet.http.HttpSession;
@@ -31,6 +34,17 @@ import lombok.extern.slf4j.Slf4j;
 public class ReviewController {
 	
 	private final ReviewService reviewService;
+	
+	//상품 이미지 업로드
+	@Value("${file.product.image.dir}")
+	private String uploadPath;
+	
+	//이미지 표시
+	@GetMapping("/image_display")
+	public ResponseEntity<byte[]> image_display(String dateFolderName, String fileName) throws Exception {
+		
+		return FileManagerUtils.getFile(uploadPath + dateFolderName, fileName);
+   }
 	
 	//리뷰목록
 	@GetMapping("/re_list/{pro_num}/{page}")
@@ -59,13 +73,21 @@ public class ReviewController {
 	
 	//상품후기 저장 consumes : 클라이언트에서 보내오는 값의 포맷(MIME)
 	@PostMapping(value = "/review_save", consumes = {"application/json"}, produces = {MediaType.TEXT_PLAIN_VALUE})
-	public ResponseEntity<String> review_save(@RequestBody ReviewVO vo, HttpSession session) throws Exception {
+	public ResponseEntity<String> review_save(@RequestBody ReviewVO vo, HttpSession session, MultipartFile uploadFile) throws Exception {
 		
 		String u_id = ((UserVO) session.getAttribute("login_status")).getU_id();
 		vo.setU_id(u_id);
 		
+		//상품 이미지 업로드
+		String dateFolder = FileManagerUtils.getDateFolder();
+		String saveFileName = FileManagerUtils.uploadFile(uploadPath, dateFolder, uploadFile);
+		
+		vo.setRe_img(saveFileName);
+		vo.setRe_up_folder(dateFolder);
+		
 		log.info("상품리뷰데이터 : " + vo);
 		
+		//리뷰 db저장
 		reviewService.review_save(vo);
 		
 		ResponseEntity<String> entity = null;
@@ -93,7 +115,8 @@ public class ReviewController {
 		
 		log.info("장바구니코드 : " + re_code);
 		ResponseEntity<ReviewVO> entity = null;
-		reviewService.review_modify(re_code);
+		ReviewVO vo = reviewService.review_modify(re_code);
+		vo.setRe_up_folder(vo.getRe_up_folder().replace("\\", "/"));
 		
 		entity = new ResponseEntity<ReviewVO>(reviewService.review_modify(re_code), HttpStatus.OK);
 		
@@ -102,8 +125,23 @@ public class ReviewController {
 	
 	//리뷰수정
 	@PutMapping("/review_modify")
-	public ResponseEntity<String> review_update(@RequestBody ReviewVO vo) throws Exception {
+	public ResponseEntity<String> review_update(@RequestBody ReviewVO vo, MultipartFile uploadFile) throws Exception {
 		ResponseEntity<String> entity = null;
+		
+		//상품 이미지 변경(업로드) 유무
+		if(!uploadFile.isEmpty()) {
+			//상품 기존 이미지삭제
+			FileManagerUtils.delete(uploadPath, vo.getRe_up_folder(), vo.getRe_img(), "image");
+			//변경이미지 업로드
+			String dateFolder = FileManagerUtils.getDateFolder();
+			String saveFileName = FileManagerUtils.uploadFile(uploadPath, dateFolder, uploadFile);
+			//새로운 파일명, 날짜폴더명
+			vo.setRe_img(saveFileName);
+			vo.setRe_up_folder(dateFolder);
+			
+			
+		}
+		
 		reviewService.review_update(vo);
 		entity = new ResponseEntity<String>("success", HttpStatus.OK);
 		
@@ -116,7 +154,8 @@ public class ReviewController {
 		
 		log.info("장바구니코드 : " + re_code);
 		ResponseEntity<ReviewVO> entity = null;
-		reviewService.review_detail(re_code);
+		ReviewVO vo = reviewService.review_detail(re_code);
+		vo.setRe_up_folder(vo.getRe_up_folder().replace("\\", "/"));
 		
 		entity = new ResponseEntity<ReviewVO>(reviewService.review_detail(re_code), HttpStatus.OK);
 		
