e7f73a3c28308ee3f8391e6a8068ca81f44aa85c
nusum2 <warfare804@gmail.com>
2025-03-05 00:17:41 +0900

    배포 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -22,7 +22,7 @@ echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 
 cd $PROJECT_FULL_PATH
 
-cp $PROJECT_FULL_PATH/portfolio/target/*.jar $JAR_FILE
+cp $PROJECT_FULL_PATH/foodmart/target/*.jar $JAR_FILE
 #sudo cp /home/ec2-user/app/step2/foodmart/target/portfolio-1.0.0.jar.jar foodmart-1.0.0.jar
 
 cd $PROJECT_FULL_PATH
