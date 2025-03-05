a49bad03eaa10279f6921b21be741d6a6d7a661f
nusum2 <warfare804@gmail.com>
2025-03-06 00:04:03 +0900

    ㄹㄹ

--- src/main/java/com/food/basic/admin/AdminController.java
+++ src/main/java/com/food/basic/admin/AdminController.java
@@ -38,11 +38,12 @@ public class AdminController {
 				
 				d_vo.setAdmin_pw("");
 				session.setAttribute("admin_state", d_vo);
-				url = "/admin/admin_menu";
+				//url = "/admin/admin_menu";
 			}
 		}
 		
-		return "redirect:" + url;
+		return "redirect:admin/admin_menu"; 
+		//+ url;
 	}
 	
 	@GetMapping("/admin_menu")
