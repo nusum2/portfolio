c1a19426b949fb5f157856bc2fb37bc108ff4a1c
nusum2 <warfare804@gmail.com>
2025-03-05 23:54:08 +0900

    수정+

--- src/main/java/com/food/basic/admin/AdminController.java
+++ src/main/java/com/food/basic/admin/AdminController.java
@@ -38,7 +38,7 @@ public class AdminController {
 				
 				d_vo.setAdmin_pw("");
 				session.setAttribute("admin_state", d_vo);
-				url = "admin/admin_menu";
+				url = "/admin/admin_menu";
 			}
 		}
 		
